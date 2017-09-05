56ea1fd43d97830a1ab37d3aa1242a672c269921
wesley willekes <wesleywillekes@MacBook-Pro-van-wesley.local>
2017-09-05 22:15:57 +0200

    Optimize chunk overlay to only use one draw call Previously I was using GL_LINE_STRIP, by using GL_LINES instead only one glBegin-glEnd block is needed.

--- src/main/java/com/wesserboy/overlays/renderers/ChunkOverlay.java
+++ src/main/java/com/wesserboy/overlays/renderers/ChunkOverlay.java
@@ -42,127 +42,94 @@ public class ChunkOverlay {
 				// translate draw origin to 0,0,0. This allows the use of world coords in draw calls.
 				GL11.glTranslated(-x, -y, -z);
 				
-				GL11.glPushMatrix();
+				//translate draw origin to the center of the chunk the player is in
+				GL11.glTranslated(player.chunkCoordX * 16 + 8, 0, player.chunkCoordZ * 16 + 8);
 				
-					//translate draw origin to the center of the chunk the player is in
-					GL11.glTranslated(player.chunkCoordX * 16 + 8, 0, player.chunkCoordZ * 16 + 8);
-					
-					GlStateManager.disableTexture2D();
-					GlStateManager.glLineWidth(1F);
-					
-					
-					// Yellow line in the middle
-					GL11.glColor3f(1F, 1F, 0F); 
+				GlStateManager.disableTexture2D();
+				GlStateManager.glLineWidth(1F);
 				
-					GL11.glBegin(GL11.GL_LINE_STRIP);
-					GL11.glVertex3d(0, 0, 0);
-					GL11.glVertex3d(0, 256, 0);
-					GL11.glEnd();
+				GL11.glBegin(GL11.GL_LINES);
+				
+				
+				// Yellow line in the middle
+				GL11.glColor3f(1F, 1F, 0F); 
+
+				GL11.glVertex3d(0, 0, 0);
+				GL11.glVertex3d(0, 256, 0);
+				
+				
+				// Red lines on corners
+				GL11.glColor3f(1F, 0F, 0F); 
+
+				GL11.glVertex3d(-8, 0, -8);
+				GL11.glVertex3d(-8, 256, -8);
+
+				GL11.glVertex3d(8, 0, 8);
+				GL11.glVertex3d(8, 256, 8);
+
+				GL11.glVertex3d(-8, 0, 8);
+				GL11.glVertex3d(-8, 256, 8);
+
+				GL11.glVertex3d(8, 0, -8);
+				GL11.glVertex3d(8, 256, -8);
+				
+				
+				if(mode == 2){
+					// Green mesh
+					GL11.glColor3f(0F, 1F, 0F);
 					
+					int minY = (int) (y - 5);
+					int maxY = (int) (y + 5);
 					
-					// Red lines on corners
-					GL11.glColor3f(1F, 0F, 0F); 
-					
-					GL11.glBegin(GL11.GL_LINE_STRIP);
-					GL11.glVertex3d(-8, 0, -8);
-					GL11.glVertex3d(-8, 256, -8);
-					GL11.glEnd();
-					
-					GL11.glBegin(GL11.GL_LINE_STRIP);
-					GL11.glVertex3d(8, 0, 8);
-					GL11.glVertex3d(8, 256, 8);
-					GL11.glEnd();
-					
-					GL11.glBegin(GL11.GL_LINE_STRIP);
-					GL11.glVertex3d(-8, 0, 8);
-					GL11.glVertex3d(-8, 256, 8);
-					GL11.glEnd();
-					
-					GL11.glBegin(GL11.GL_LINE_STRIP);
-					GL11.glVertex3d(8, 0, -8);
-					GL11.glVertex3d(8, 256, -8);
-					GL11.glEnd();
-					
-					
-					if(mode == 2){
-						// Green mesh
-						GL11.glColor3f(0F, 1F, 0F);
-						
-						int minY = (int) (y - 5);
-						int maxY = (int) (y + 5);
-						
-						// Vertical
-						for(int i = minY; i <= maxY; i++){
-							GL11.glBegin(GL11.GL_LINE_STRIP);
-							GL11.glVertex3d(-8, i, -8);
-							GL11.glVertex3d(-8, i, 8);
-							GL11.glEnd();
-							
-							GL11.glBegin(GL11.GL_LINE_STRIP);
-							GL11.glVertex3d(-8, i, 8);
-							GL11.glVertex3d(8, i, 8);
-							GL11.glEnd();
-							
-							GL11.glBegin(GL11.GL_LINE_STRIP);
-							GL11.glVertex3d(8, i, 8);
-							GL11.glVertex3d(8, i, -8);
-							GL11.glEnd();
-							
-							GL11.glBegin(GL11.GL_LINE_STRIP);
-							GL11.glVertex3d(8, i, -8);
-							GL11.glVertex3d(-8, i, -8);
-							GL11.glEnd();
-						}
-						
-						// Horizontal
-						for(int i = 0; i < 8; i++){
-							
-							GL11.glBegin(GL11.GL_LINE_STRIP);
-							GL11.glVertex3d(-8, minY, -i);
-							GL11.glVertex3d(-8, maxY, -i);
-							GL11.glEnd();
-							
-							GL11.glBegin(GL11.GL_LINE_STRIP);
-							GL11.glVertex3d(-i, minY, -8);
-							GL11.glVertex3d(-i, maxY, -8);
-							GL11.glEnd();
-							
-							GL11.glBegin(GL11.GL_LINE_STRIP);
-							GL11.glVertex3d(8, minY, i);
-							GL11.glVertex3d(8, maxY, i);
-							GL11.glEnd();
-							
-							GL11.glBegin(GL11.GL_LINE_STRIP);
-							GL11.glVertex3d(i, minY, 8);
-							GL11.glVertex3d(i, maxY, 8);
-							GL11.glEnd();
-							
-							GL11.glBegin(GL11.GL_LINE_STRIP);
-							GL11.glVertex3d(8, minY, -i);
-							GL11.glVertex3d(8, maxY, -i);
-							GL11.glEnd();
-							
-							GL11.glBegin(GL11.GL_LINE_STRIP);
-							GL11.glVertex3d(i, minY, -8);
-							GL11.glVertex3d(i, maxY, -8);
-							GL11.glEnd();
-							
-							GL11.glBegin(GL11.GL_LINE_STRIP);
-							GL11.glVertex3d(-8, minY, i);
-							GL11.glVertex3d(-8, maxY, i);
-							GL11.glEnd();
-							
-							GL11.glBegin(GL11.GL_LINE_STRIP);
-							GL11.glVertex3d(-i, minY, 8);
-							GL11.glVertex3d(-i, maxY, 8);
-							GL11.glEnd();
-							 
-						}
+					// Vertical
+					for(int i = minY; i <= maxY; i++){
+
+						GL11.glVertex3d(-8, i, -8);
+						GL11.glVertex3d(-8, i, 8);
+
+						GL11.glVertex3d(-8, i, 8);
+						GL11.glVertex3d(8, i, 8);
+
+						GL11.glVertex3d(8, i, 8);
+						GL11.glVertex3d(8, i, -8);
+
+						GL11.glVertex3d(8, i, -8);
+						GL11.glVertex3d(-8, i, -8);
 					}
 					
-					GlStateManager.enableTexture2D();
+					// Horizontal
+					for(int i = 0; i < 8; i++){
+
+						GL11.glVertex3d(-8, minY, -i);
+						GL11.glVertex3d(-8, maxY, -i);
+
+						GL11.glVertex3d(-i, minY, -8);
+						GL11.glVertex3d(-i, maxY, -8);
+
+						GL11.glVertex3d(8, minY, i);
+						GL11.glVertex3d(8, maxY, i);
+
+						GL11.glVertex3d(i, minY, 8);
+						GL11.glVertex3d(i, maxY, 8);
+
+						GL11.glVertex3d(8, minY, -i);
+						GL11.glVertex3d(8, maxY, -i);
+
+						GL11.glVertex3d(i, minY, -8);
+						GL11.glVertex3d(i, maxY, -8);
+
+						GL11.glVertex3d(-8, minY, i);
+						GL11.glVertex3d(-8, maxY, i);
+
+						GL11.glVertex3d(-i, minY, 8);
+						GL11.glVertex3d(-i, maxY, 8);
+						 
+					}
+				}
 				
-				GL11.glPopMatrix();
+				GL11.glEnd();
+				
+				GlStateManager.enableTexture2D();
 			
 			GL11.glPopMatrix();
 		}
